20fdcb87bf5b3b850aa3564c9d52fe2a384a6a5b
Ubuntu <ubuntu@ip-172-31-32-144.eu-central-1.compute.internal>
2022-05-27 10:58:04 +0000

    finish

--- access_2ha.sh
+++ access_2ha.sh
@@ -3,23 +3,31 @@
 user=$(echo $USER)
 access=$(grep $USER /etc/ssh/two-factor-skip.conf | awk '{print$1}')
 add="$user ALL=(ALL) NOPASSWD:/usr/sbin/useradd,/usr/sbin/userdel,/usr/sbin/usermod,/var/log/syslog"
-nopass=$(sudo cat /etc/sudoers.d/$user | grep NOPASSWD:AL | awk '{print$3}')
+#nopass=$(sudo cat /etc/sudoers.d/$user | grep NOPASSWD:AL | awk '{print$3}')
 del="NOPASSWD:ALL"
+search=$(sudo -l -U $user | grep "NOPASSWD: ALL" | awk '{print$3}')
+all="ALL"
+
 
 if [ "$access" == "+" ]
  then
  sudo sed -i "/$user/d" /etc/ssh/two-factor-skip.conf
 fi
 
-if [ "$nopass" == "$del" ]
+if [ "$search" != "$all" ]
  then
-  sudo sh -c "echo '$add' > /etc/sudoers.d/$user"
- else
  echo ""
+ elif [ "$search" == "$all" ]
+ then
+ sudo sh -c "echo '$add' > /etc/sudoers.d/$user"
 fi
+
 
 if [ "$access" == "+" ]
  then
   exec "/usr/bin/google-authenticator"
 fi
+
+
+
 
