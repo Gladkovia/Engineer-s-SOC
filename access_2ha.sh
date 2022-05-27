2db9eee224757b78c9169e8429a256a53a845892
Ubuntu <ubuntu@ip-172-31-32-144.eu-central-1.compute.internal>
2022-05-27 10:58:46 +0000

    finish1

--- access_2ha.sh
+++ access_2ha.sh
@@ -3,7 +3,6 @@
 user=$(echo $USER)
 access=$(grep $USER /etc/ssh/two-factor-skip.conf | awk '{print$1}')
 add="$user ALL=(ALL) NOPASSWD:/usr/sbin/useradd,/usr/sbin/userdel,/usr/sbin/usermod,/var/log/syslog"
-#nopass=$(sudo cat /etc/sudoers.d/$user | grep NOPASSWD:AL | awk '{print$3}')
 del="NOPASSWD:ALL"
 search=$(sudo -l -U $user | grep "NOPASSWD: ALL" | awk '{print$3}')
 all="ALL"
