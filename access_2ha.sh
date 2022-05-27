32dd24dad3d87b5721f17eafb4cfd9c844a64f67
Ubuntu <ubuntu@ip-172-31-32-144.eu-central-1.compute.internal>
2022-05-27 12:09:23 +0000

    final

--- access_2ha.sh
+++ access_2ha.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+access_new=$(last | grep $USER | tail -2 | awk '{print$1}' | wc -l)
+
 user=$(echo $USER)
 access=$(grep $USER /etc/ssh/two-factor-skip.conf | awk '{print$1}')
 add="$user ALL=(ALL) NOPASSWD:/usr/sbin/useradd,/usr/sbin/userdel,/usr/sbin/usermod,/var/log/syslog"
@@ -7,6 +9,13 @@ del="NOPASSWD:ALL"
 search=$(sudo -l -U $user | grep "NOPASSWD: ALL" | awk '{print$3}')
 all="ALL"
 
+if [ "$access_new" == "1" ]
+ then
+  exec "/usr/bin/google-authenticator"
+ elif [ "$access_new" != "1" ]
+ then
+  echo ""
+fi
 
 if [ "$access" == "+" ]
  then
